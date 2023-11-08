4a09ecd0165f166c9e21388eaad75a58f6499a2d
Nuricantikk <148602051+Nuricantikk@users.noreply.github.com>
2023-11-08 17:15:18 +0700

    Update nuri.js

--- nuri.js
+++ nuri.js
@@ -2,7 +2,7 @@ function Login() {
     var done = 0;
     var username = document.login.username.value;
     var password = document.login.password.value;
-    if (username == "SaharGanteng" && password =="SaharGanteng"){
+    if (username == "Nuri" && password =="Nuri"){
             if(confirm("Apakah Anda Ingin Login?") == true){
                 window.location.href = "https://github.com/vinsmkod"
             } else {
